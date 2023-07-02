daeb5362878f3280024a13fed1ff1f71f1657431
duc minhh <ducminhh219@gmail.com>
2023-07-02 15:10:09 +0700

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
-// close navbar on hamburger menu
-$(document).click(function (event) {
+// close hamburger menu on click
+$(document).click((event) => {
   if (
     $(".navbar-collapse").hasClass("show") === true &&
     !$(event.target).hasClass("navbar-toggler")
   ) {
     $(".navbar-toggler").click();
   }
+});
+
+$(".carousel").swipe({
+
+  swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+
+    if (direction == 'left') $(this).carousel('next');
+    if (direction == 'right') $(this).carousel('prev');
+
+  },
+  allowPageScroll:"vertical"
+
 });
