f7e589efa2c2c8d0fdf1ed8191ee9decce93d8d0
duc minhh <ducminhh219@gmail.com>
2023-07-03 01:10:40 +0700

    not done yet but i need sleep :(

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,3 @@ $(document).click((event) => {
     $(".navbar-toggler").click();
   }
 });
-
-$(".carousel").swipe({
-
-  swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
-
-    if (direction == 'left') $(this).carousel('next');
-    if (direction == 'right') $(this).carousel('prev');
-
-  },
-  allowPageScroll:"vertical"
-
-});
